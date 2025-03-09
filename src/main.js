20daeeed650579745bd89df905a1b687cdd78625
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2025-03-09 11:55:54 -0400

    Implement tensor initialization

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import './style.css'
 import {Tensor} from "../lib/main.js";
 
 
-const t = new Tensor([3, 3], true);
+const t = new Tensor([[1, 2, 3], [4, 5, 6]]);
 console.log(t.toString());
+console.log(t.data);
+console.log(t.shape);
 
 document.querySelector('#app').innerHTML = `
   <div>
